80c4773a8e6b467eafce9489310999bab27c80e0
Anders Olesen <anders242@gmail.com>
2021-11-25 14:14:39 +0000

    add ispalindrome aldorithm

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
-function harmlessRansomNote(noteText, magazineText) {
-    const noteArray = noteText.split(' ')
-    const magazineArray = magazineText.split(' ')
-    const magazineObj = {}
-
-    magazineArray.forEach(word => { if(!magazineObj[word]) magazineObj[word] = 0;
-        magazineObj[word]++
-    }
-    ) 
-    let noteIsPossible = true
-      
-    noteArray.forEach(word => {
-        if(magazineObj[word]) { magazineObj[word]--
-        if(magazineObj[word] < 0) noteIsPossible = false
-        } else noteIsPossible = false
-    }
-        )  
-        return noteIsPossible 
-
+function isPalindrome (str) {
+    strArr = str.split('')
+    console.log(strArr)
 }
 
-console.log(harmlessRansomNote('this is my my simple note', 'this is my simple note lets try with a different '))
+
+console.log(isPalindrome('race car'))
