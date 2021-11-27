c9c5f21d35cff5a320ae0c1d28e1c14f84f2423e
Anders Olesen <anders242@gmail.com>
2021-11-27 08:40:04 +0000

    ceasercipher

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-function isPalindrome (str) {
-    strArr = str.split('')
-    console.log(strArr)
-}
+ function ceasarCipher (str, num) {
+     let lowerCaseString = str.toLowerCase()
+     const alphabet = 'abcdefghijklmnopqrstuvxyz'.split('')
+     let newString = ''
 
+     for (let i = 0 ; i < str.length ; i ++) {
+         let currentChar = lowerCaseString[i]
+         if(str[i] === '') {
+             newString += currentChar
+             continue
+         }
+         let currentLetterIndex = alphabet.indexOf(currentChar)
+         let newIndex = currentLetterIndex + num
 
-console.log(isPalindrome('race car'))
+         console.log(currentLetterIndex) 
+         console.log(newIndex) 
+         console.log(newString)
+         return newString += alphabet[newIndex]
+        }
+ }
+
+ console.log(ceasarCipher('abcdefg', 3))
