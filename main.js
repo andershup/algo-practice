1f9f82ad9545ca23689242ddd94c58b4cfcb8679
Anders Olesen <anders242@gmail.com>
2021-11-29 11:52:17 +0000

    add factorial function

--- main.js
+++ main.js
@@ -61,20 +61,31 @@
 
 // console.log(getMode([1,1,1,1,1,2,3,4,4,4,4,4,4,4,5,6,7,8,9,9,9,,]))
 
-function twoSum(numArray, sum) {
-    var pairs = [];
-    var hashTable = [];
+// function twoSum(numArray, sum) {
+//     var pairs = [];
+//     var hashTable = [];
    
-    for (var i = 0; i < numArray.length; i++) {
-      var currNum = numArray[i];
-      var counterpart = sum - currNum;
-      if (hashTable.indexOf(counterpart) !== -1) {
-        pairs.push([ currNum, counterpart ]);
-      }
-      hashTable.push(currNum);
-    }
+//     for (var i = 0; i < numArray.length; i++) {
+//       var currNum = numArray[i];
+//       var counterpart = sum - currNum;
+//       if (hashTable.indexOf(counterpart) !== -1) {
+//         pairs.push([ currNum, counterpart ]);
+//       }
+//       hashTable.push(currNum);
+//     }
     
-    return pairs;
-  }
+//     return pairs;
+//   }
    
-  twoSum([1, 6, 4, 5, 3, 3], 7);
+//   twoSum([1, 6, 4, 5, 3, 3], 7);
+
+//   function factorial (num) {
+//       if(num === 1) {
+//           return num
+//       }
+//       else {
+//           return num * factorial(num - 1) 
+//       }
+//   }
+  
+//   console.log(factorial(4))
