28922a95ec861e769b3dcf2c6f7fe4053aac515b
andershup <anders242@gmail.com>
2022-01-19 11:42:12 +0000

    add updated ransom note algo

--- main.js
+++ main.js
@@ -444,3 +444,34 @@
 
 // console.log(isPalindrome('racecar'))
 
+
+function harmlessRansomNote(note, magazine) {
+    noteArray = note.toLowerCase().split(' ')
+
+    magazineArray = magazine.toLowerCase().split(' ')
+  
+    hashTable = {}
+  
+    magazineArray.forEach(element => {
+        if(!hashTable[element]) hashTable[element] = 0
+        hashTable[element]++
+    });
+    console.log(hashTable)
+    noteIsPossible = true
+    
+    noteArray.forEach(element => {
+        if(!hashTable[element]) return noteIsPossible = false
+        if(hashTable[element]) {
+            hashTable[element]--
+            if(hashTable[element] < 0) return noteIsPossible = false
+        }
+        else noteIsPossible = true
+        console.log(hashTable)
+    }) 
+
+    return noteIsPossible
+
+}
+
+
+console.log(harmlessRansomNote('this is a secret note for you from an admirer', 'puerto rico is a place of great wonder and excitement it has many  waterfall locations that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited'));
