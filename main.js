b66498fd7c98fefdefb24fc6d5e76bf16d88d8ae
andershup <anders242@gmail.com>
2022-02-09 13:20:24 +0000

    first occuring

--- main.js
+++ main.js
@@ -1,52 +1,12 @@
-class HashTable {
-    constructor(size){
-      this.data = new Array(size);
-      // this.data = [];
-    }
-  
-    _hash(key) {
-      let hash = 0;
-      for (let i =0; i < key.length; i++){
-          hash = (hash + key.charCodeAt(i) * i) % this.data.length
-      }
-      return hash;
-    }
-    set(key, value) {
-        let address = this._hash(key)
-        if(!this.data[address]) {
-            this.data[address] = []
+const firstOccuring = (arr)  => {
+    let allreadyDone = []
+    for(i = 0 ; i < arr.length ; i ++) {
+        let currentNum = arr[i]
+        if( allreadyDone.indexOf(currentNum) !== -1) {
+            return currentNum
         }
-        this.data[address].push([key, value])
-      
-        return this.data
+        allreadyDone.push(currentNum)
     }
-    get(key) {
-        let address = this._hash(key)
-        const currentBucket = this.data[address]
-        if(currentBucket) {
-            for(let i = 0 ; i < currentBucket.length ; i ++) {
-                if(currentBucket[i] [0] === key) {
-                    return currentBucket[i][1]
-                }
-            }
-        }
-    }
-    keys() {
-        const keysArray = []
-        for(let i = 0 ; i < this.data.length ; i ++) {
-            keysArray.push(this.data[i][0])
-            
-        }
-        return keysArray
-
-    }
-
 }
-   
-const myHashTable = new HashTable(50)
-console.log(myHashTable.set('grapes', 1000))
-console.log(myHashTable.set('oranges', 1))
-console.log(myHashTable.set('apples', 10))
-console.log(myHashTable.set('teeth', 100))
-// console.log(myHashTable.get('grapes'))
-console.log(myHashTable.keys())
+
+console.log(firstOccuring([1,2,3,4,5,6,7,8,9,4]))
