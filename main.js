59bfa580bbc5d133fe135975010f8d89e1d70816
andershup <anders242@gmail.com>
2022-02-09 07:47:39 +0000

    add array manipulaton practice

--- main.js
+++ main.js
@@ -123,62 +123,73 @@
 // let arrayOne = [1,2,3,4,5,6,7,8]
 
 
-// let arrayTwo = arrayOne.concat().reverse()
-// console.log(arrayOne)
-// console.log(arrayTwo)
+// // let arrayTwo = arrayOne.concat().reverse()
+// // console.log(arrayOne)
+// // console.log(arrayTwo)
 
-function getMean(array) {
-    let sum = 0 
-    array.forEach(num => {
-        sum += num
-    })
-    let result = sum / array.length
-    return result 
-}
+// function getMean(array) {
+//     let sum = 0 
+//     array.forEach(num => {
+//         sum += num
+//     })
+//     let result = sum / array.length
+//     return result 
+// }
 
-function getMedian(array) {
-    array.sort(function(a, b) { a-b })
-    let median
-    if(array.length % 2 !== 0) {
-        median = array[Math.floor(array.length/2)]
-    }
-    else 
-    {
-        let bottomNumber = array[(array.length / 2) -1]
-        let topNumber = array[(array.length)]
-        median = (bottomNumber + topNumber) / 2
-    }
-    return median
+// function getMedian(array) {
+//     array.sort(function(a, b) { a-b })
+//     let median
+//     if(array.length % 2 !== 0) {
+//         median = array[Math.floor(array.length/2)]
+//     }
+//     else 
+//     {
+//         let bottomNumber = array[(array.length / 2) -1]
+//         let topNumber = array[(array.length)]
+//         median = (bottomNumber + topNumber) / 2
+//     }
+//     return median
     
-}
+// }
 
-function getMode(array) {
-    let newObj = {}
+// function getMode(array) {
+//     let newObj = {}
 
-    array.forEach(num => {
-        if(!newObj[num]) newObj[num] = 0
-        newObj[num] ++
-        })
-    let maxNumber = 0 
-    modes = []
-    for(num in newObj) {
-        if(newObj[num] > maxNumber) {
-            modes = [num]
-            maxNumber = newObj[num]
-        }
-    }
-    return modes
+//     array.forEach(num => {
+//         if(!newObj[num]) newObj[num] = 0
+//         newObj[num] ++
+//         })
+//     let maxNumber = 0 
+//     modes = []
+//     for(num in newObj) {
+//         if(newObj[num] > maxNumber) {
+//             modes = [num]
+//             maxNumber = newObj[num]
+//         }
+//     }
+//     return modes
    
-}
+// }
 
-console.log(getMode([1,2,2,3,4,5,5,5,5]))
+// console.log(getMode([1,2,2,3,4,5,5,5,5]))
 
 
 
-function meanmedianmode(array) {
-    return {
-        mean: getMean(array),
-        median: getMedian(array),
-        mode: getMode(array)
+// function meanmedianmode(array) {
+//     return {
+//         mean: getMean(array),
+//         median: getMedian(array),
+//         mode: getMode(array)
+//     }
+// }
+
+function reverseArray(array) {
+    for(i = 0 ; i < array.length/2 ; i++) {
+        let tempVar = array[i]
+        array[i] = array[array.length -1 -i]
+        array[array.length - 1 - i] = tempVar
     }
+    return array 
 }
+
+console.log(reverseArray([1,2,3,4,5,6,7]))
