563fb618d7b06319c1db1cab4186e1e6a55277cd
andershup <anders242@gmail.com>
2022-02-10 09:14:24 +0000

    algo practice

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const firstOccuring = (arr)  => {
-    let allreadyDone = []
-    for(i = 0 ; i < arr.length ; i ++) {
-        let currentNum = arr[i]
-        if( allreadyDone.indexOf(currentNum) !== -1) {
-            return currentNum
-        }
-        allreadyDone.push(currentNum)
+const twoSum = (numArray, sum) => {
+    resultArray = []
+    let hashTable = []
+    let pairs = []
+    for(i = 0 ; i < numArray.length ; i++) {
+        let currentNumber = numArray[i]
+        let opposite = sum - numArray[i]
+        if(hashTable.indexOf(opposite) !== -1) {
+            pairs.push([currentNumber, opposite])
+        } 
+        hashTable.push(currentNumber)
     }
+    resultArray.push(pairs)
+    return resultArray
 }
 
-console.log(firstOccuring([1,2,3,4,5,6,7,8,9,4]))
+console.log(twoSum([1,2,3,4,5,6,7,8,9], 10))
