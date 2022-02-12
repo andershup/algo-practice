c588a65cc7aef0017a6206a029e11361a9ad449a
andershup <anders242@gmail.com>
2022-02-12 08:47:57 +0000

    array manipulation practice

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-const twoSum = (numArray, sum) => {
-    resultArray = []
-    let hashTable = []
-    let pairs = []
-    for(i = 0 ; i < numArray.length ; i++) {
-        let currentNumber = numArray[i]
-        let opposite = sum - numArray[i]
-        if(hashTable.indexOf(opposite) !== -1) {
-            pairs.push([currentNumber, opposite])
-        } 
-        hashTable.push(currentNumber)
-    }
-    resultArray.push(pairs)
-    return resultArray
-}
+// class HashTable {
+//     constructor(size){
+//       this.data = new Array(size);
+//     }
+  
+//     _hash(key) {
+//       let hash = 0;
+//       for (let i =0; i < key.length; i++){
+//           hash = (hash + key.charCodeAt(i) * i) % this.data.length
+//       }
+//       return hash;
+//     }
+  
 
-console.log(twoSum([1,2,3,4,5,6,7,8,9], 10))
+// }
+
+  
+//   const myHashTable = new HashTable(50);
+//  console.log( myHashTable.set('grapes', 10000))
+//  console.log( myHashTable.get('grapes'))
+//   myHashTable.set('apples', 9)
+//  console.log(myHashTable.get('apples'))
+
+
+
+// const reverseArray = (arr) => {
+//     for(let i = 0 ; i < arr.length/2 ; i++) {
+//         let tempVar = arr[i]
+//         arr[i] = arr[arr.length -1 - i]
+//         console.log(arr[i])
+//         arr[arr.length -1 - i] = tempVar
+//     }
+//     return arr
+
+// }
+
+// console.log(reverseArray([1,2,3,4,5,6,7,8,9]))
