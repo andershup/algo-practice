00046702e96088f219d67aa614110c4113369bd5
andershup <anders242@gmail.com>
2022-02-15 10:59:57 +0000

    add linked list with methods

--- main.js
+++ main.js
@@ -133,4 +133,64 @@
 //   }
 //   return stringReversal(str.substr(1)) + str.charAt()
 // }
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value,
+      next: null
+    }
+    this.tail = this.head
+    this.length = 1 
+  }
+  append(value) {
+    const newNode = new Node(value)
+    this.tail.next = newNode
+    this.tail = newNode
+    this.length++
+    return this
+  }
+  prepend(value) {
+    const newNode = new Node(value)
+    newNode.next = this.head
+    this.head = newNode
+    this.length ++
+    return this
+  }
+  printList() {
+    console.log(this.head)
+    const array = []
+    let currentNode = this.head
+    while(currentNode !== null) {
+      array.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return array 
+  }
+  insert(index, value) {
+    let array = []
+    const newNode = new Node(value)
+    let currentNode = this.head
+    while(currentNode !== null) {
+      array.push(currentNode.value)
+      currentNode = currentNode.next
+    }
 
+    
+
+  }
+}
+
+
+const myNewList = new LinkedList(10)
+myNewList.append(55)
+myNewList.append(555)
+myNewList.prepend(22)
+console.log(myNewList)
+// console.log(myNewList.insert(2,22))
+console.log(myNewList.printList())
