d31364aaae17df1cc13fffc9211edf83b8b23d65
andershup <anders242@gmail.com>
2022-03-10 12:47:41 +0000

    add tree data structure with insert and loopup methods

--- main.js
+++ main.js
@@ -1468,3 +1468,89 @@
 //   }
 //   return cache[index];
 // }
+
+class Node {
+  constructor(value){
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null;
+  }
+  insert(value){
+    let newNode = new Node(value)
+    if(this.root === null)
+    {
+      this.root = newNode
+      return this
+    }
+   else {
+     let currentNode = this.root
+     while(true) {
+       if(value < currentNode.value) 
+       {
+         if(!currentNode.left)
+         {
+           currentNode.left = newNode
+           return this
+         }
+         currentNode = currentNode.left
+       } else {
+         if(!currentNode.right) 
+         {
+           currentNode.right = newNode
+           return this
+         }
+         currentNode = currentNode.right
+       }
+     }
+   }
+  }
+   
+  lookup(value){
+    //Code here
+    let currentNode = this.root
+    while(currentNode.value !== value) 
+    {
+      if(value < currentNode.value)
+      {
+        currentNode = currentNode.left
+      }
+      else {
+        currentNode = currentNode.right
+      }
+    }
+    return currentNode
+  }
+  remove(value) {
+    let currentNode = this.root
+    if()
+  }
+  // remove
+}
+
+const tree = new BinarySearchTree();
+console.log(tree.insert(9))
+console.log(tree.insert(4))
+console.log(tree.insert(6))
+console.log(tree.insert(20))
+console.log(tree.insert(170))
+console.log(tree.insert(15))
+console.log(tree.insert(1))
+console.log(tree.lookup(170))
+console.log(JSON.stringify(traverse(tree.root)))
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
