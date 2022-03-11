1ddb7a712ac5756fcd62d313edca821ac553d7b5
andershup <anders242@gmail.com>
2022-03-11 09:43:26 +0000

    add graph showconnections method

--- main.js
+++ main.js
@@ -1584,55 +1584,55 @@
 // }
 // console.log(twoSum([1,2,3,4,5,6,7,8,9,0], 10))
 
-class Graph { 
-  constructor() { 
-    this.numberOfNodes = 0;
-    this.adjacentList = {
-    }; 
-  } 
-  addVertex(node)  {
-   this.adjacentList[node] = []
-   this.numberOfNodes ++
-  } 
-  addEdge(node1, node2) { 
-    //undirected Graph 
-    this.adjacentList[node1].push(node2)
-    this.adjacentList[node2].push(node1)
+// class Graph { 
+//   constructor() { 
+//     this.numberOfNodes = 0;
+//     this.adjacentList = {
+//     }; 
+//   } 
+//   addVertex(node)  {
+//    this.adjacentList[node] = []
+//    this.numberOfNodes ++
+//   } 
+//   addEdge(node1, node2) { 
+//     //undirected Graph 
+//     this.adjacentList[node1].push(node2)
+//     this.adjacentList[node2].push(node1)
   
     
-  } 
-  showConnections() { 
-    const allNodes = Object.keys(this.adjacentList); 
-    for (let node of allNodes) { 
-      let nodeConnections = this.adjacentList[node]; 
-      let connections = ""; 
-      let vertex;
-      for (vertex of nodeConnections) {
-        connections += vertex + " ";
-      } 
-      console.log(node + "-->" + connections); 
-    } 
-} 
-} 
+//   } 
+//   showConnections() { 
+//     const allNodes = Object.keys(this.adjacentList); 
+//     for (let node of allNodes) { 
+//       let nodeConnections = this.adjacentList[node]; 
+//       let connections = ""; 
+//       let vertex;
+//       for (vertex of nodeConnections) {
+//         connections += vertex + " ";
+//       } 
+//       console.log(node + "-->" + connections); 
+//     } 
+// } 
+// } 
 
-const myGraph = new Graph();
-console.log(myGraph.addVertex('0'))
-console.log(myGraph.addVertex('1'))
-console.log(myGraph.addVertex('2'))
-console.log(myGraph.addVertex('3'))
-console.log(myGraph.addVertex('4'))
-console.log(myGraph.addVertex('5'))
-console.log(myGraph.addVertex('6'))
-console.log(myGraph.addEdge('3', '1'))
-console.log(myGraph.addEdge('3', '4'))
-console.log(myGraph.addEdge('4', '2'))
-console.log(myGraph.addEdge('4', '5'))
-console.log(myGraph.addEdge('1', '2'))
-console.log(myGraph.addEdge('1', '0'))
-console.log(myGraph.addEdge('0', '2'))
-console.log(myGraph.addEdge('6', '5'))
+// const myGraph = new Graph();
+// console.log(myGraph.addVertex('0'))
+// console.log(myGraph.addVertex('1'))
+// console.log(myGraph.addVertex('2'))
+// console.log(myGraph.addVertex('3'))
+// console.log(myGraph.addVertex('4'))
+// console.log(myGraph.addVertex('5'))
+// console.log(myGraph.addVertex('6'))
+// console.log(myGraph.addEdge('3', '1'))
+// console.log(myGraph.addEdge('3', '4'))
+// console.log(myGraph.addEdge('4', '2'))
+// console.log(myGraph.addEdge('4', '5'))
+// console.log(myGraph.addEdge('1', '2'))
+// console.log(myGraph.addEdge('1', '0'))
+// console.log(myGraph.addEdge('0', '2'))
+// console.log(myGraph.addEdge('6', '5'))
 
-console.log(myGraph.showConnections())
+// console.log(myGraph.showConnections())
 //Answer:
 // 0-->1 2 
 // 1-->3 2 0 
@@ -1641,3 +1641,62 @@ console.log(myGraph.showConnections())
 // 4-->3 2 5 
 // 5-->4 6 
 // 6-->5
+
+// class Graph { 
+//   constructor() { 
+//     this.numberOfNodes = 0;
+//     this.adjacentList = {
+//     }; 
+//   } 
+//   addVertex(node)  { 
+//     if(!this.adjacentList[node]) this.adjacentList[node] = []
+//     this.numberOfNodes ++
+//     return this
+//   } 
+//   addEdge(node1, node2) { 
+//     //undirected Graph 
+//     this.adjacentList[node1].push(node2)
+//     this.adjacentList[node2].push(node1)
+//     return this
+
+//   } 
+//   showConnections() { 
+//     const allNodes = Object.keys(this.adjacentList); 
+//     //setting up an nested loop
+//     for (let node of allNodes) { 
+//       let nodeConnections = this.adjacentList[node]; 
+//       let connections = ""; 
+//       let vertex;
+//       for (vertex of nodeConnections) {
+//         connections += vertex + " ";
+//       } 
+//       console.log(node + "-->" + connections); 
+//     } 
+//   }
+// }
+// const myGraph = new Graph();
+// console.log(myGraph.addVertex('0'))
+// console.log(myGraph.addVertex('1'))
+// console.log(myGraph.addVertex('2'))
+// console.log(myGraph.addVertex('3'))
+// console.log(myGraph.addVertex('4'))
+// console.log(myGraph.addVertex('5'))
+// console.log(myGraph.addVertex('6'))
+// console.log(myGraph.addEdge('3', '1'))
+// console.log(myGraph.addEdge('3', '4'))
+// console.log(myGraph.addEdge('4', '2'))
+// console.log(myGraph.addEdge('4', '5'))
+// console.log(myGraph.addEdge('1', '2'))
+// console.log(myGraph.addEdge('1', '0'))
+// console.log(myGraph.addEdge('0', '2'))
+// console.log(myGraph.addEdge('6', '5'))
+
+// myGraph.showConnections(); 
+// //Answer:
+// // 0-->1 2 
+// // 1-->3 2 0 
+// // 2-->4 1 0 
+// // 3-->1 4 
+// // 4-->3 2 5 
+// // 5-->4 6 
+// // 6-->5
