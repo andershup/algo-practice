42e2c7d15f70cfe33a84a603cb45c61e318fcc4b
andershup <anders242@gmail.com>
2022-03-16 09:56:19 +0000

    add showconnctions method to graph data structure

--- main.js
+++ main.js
@@ -479,20 +479,89 @@
 //google
 
 
-const numbers = [99,44,6,2,1,5,63,87,283,4,0]
+// const numbers = [99,44,6,2,1,5,63,87,283,4,0]
 
-const reverseArray = array => {
-  for(i = 0 ; i < array.length/2 ; i++) {
-    let temp = array[i]
-    let opposite = array[array.length - 1 - i]
-    array[i] = opposite
-    array[array.length - 1 - i] = temp 
-  }
-  return array
-}
+// const reverseArray = array => {
+//   for(i = 0 ; i < array.length/2 ; i++) {
+//     let temp = array[i]
+//     let opposite = array[array.length - 1 - i]
+//     array[i] = opposite
+//     array[array.length - 1 - i] = temp 
+//   }
+//   return array
+// }
 
-console.log(reverseArray(numbers))
+// console.log(reverseArray(numbers))
 
 
+class Graph { 
+  constructor() { 
+    this.numberOfNodes = 0;
+    this.adjacentList = {
+    }; 
+  } 
+  addVertex(node)  {
+    if(!this.adjacentList[node]) this.adjacentList[node] = []
+    return this
 
+  } 
+  addEdge(node1, node2) { 
+    //undirected Graph 
+    this.adjacentList[node1].push(node2)
+    this.adjacentList[node2].push(node1)
+  } 
+//   showConnections() { 
+//     const allNodes = Object.keys(this.adjacentList); 
+//     for (let node of allNodes) { 
+//       let nodeConnections = this.adjacentList[node]; 
+//       let connections = ""; 
+//       let vertex;
+//       for (vertex of nodeConnections) {
+//         connections += vertex + " ";
+//       } 
+//       console.log(node + "-->" + connections); 
+//     } 
+// } 
+// showConnections() {
+//   let allNodes = Object.keys(this.adjacentList)
+//   for(let node of allNodes) {
+//     let nodeConnnections = this.adjacentList[node]
+    
+//     let connections = ''
+//     let vertex 
+//     for(vertex of nodeConnnections) {
+//       connections += vertex + ' '
+     
+//     } console.log(node + '-->' + connections)
+//   }
+// }
+// } 
+
+// const myGraph = new Graph();
+// console.log(myGraph.addVertex('0'))
+// console.log(myGraph.addVertex('1'))
+// console.log(myGraph.addVertex('2'))
+// console.log(myGraph.addVertex('3'))
+// console.log(myGraph.addVertex('4'))
+// console.log(myGraph.addVertex('5'))
+// console.log(myGraph.addVertex('6'))
+
+// console.log(myGraph.addEdge('3', '1'))
+// console.log(myGraph.addEdge('3', '4'))
+// console.log(myGraph.addEdge('4', '2'))
+// console.log(myGraph.addEdge('4', '5'))
+// console.log(myGraph.addEdge('1', '2'))
+// console.log(myGraph.addEdge('1', '0'))
+// console.log(myGraph.addEdge('0', '2'))
+// console.log(myGraph.addEdge('6', '5'))
+
+// console.log(myGraph.showConnections())
+//Answer:
+// 0-->1 2 
+// 1-->3 2 0 
+// 2-->4 1 0 
+// 3-->1 4 
+// 4-->3 2 5 
+// 5-->4 6 
+// 6-->5
 
