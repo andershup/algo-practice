692bc6eca7cb0086d3cae81a84b3b74a13110224
andershup <anders242@gmail.com>
2022-03-22 10:10:37 +0000

    add hash table methods

--- main.js
+++ main.js
@@ -565,18 +565,34 @@
 // 5-->4 6 
 // 6-->5
 
-const numbers = [99,44,6,2,1,5,63,87,283,4,0]
+class HashTable {
+  constructor(size){
+    this.data = new Array(size);
+  }
 
-const bubbleSort = array => {
-  for(i = 0 ; i < array.length ; i ++) {
-    for(j = 0 ; j < array.length ; j ++) {
-      if(array[j] < array[i]) {
-        let temp = array[i]
-        array[i] =  array[j]
-        array[j] = temp
-      }
+  _hash(key) {
+    let hash = 0;
+    for (let i =0; i < key.length; i++){
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    }
+    return hash;
+  }
+  set(key, value) {
+    let address = this._hash(key)
+    if(!this.data[address]) this.data[address] = []
+    this.data[address].push([key,value])
+    return this
+  }
+  get(key) {
+    let address = this._hash(key)
+    if(this.data[address]) {
+      return this.data[address]
     }
   }
-  return array
 }
-console.log(bubbleSort(numbers))
+
+const myHashTable = new HashTable(50);
+console.log(myHashTable.set('grapes', 10000))
+console.log(myHashTable.set('apples', 9))
+console.log(myHashTable.get('grapes'))
+console.log(myHashTable.get('apples'))
