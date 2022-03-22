841116c361613c1e48e84f579fc7075a2e35fb68
andershup <anders242@gmail.com>
2022-03-22 10:15:43 +0000

    factoral practice

--- main.js
+++ main.js
@@ -565,34 +565,42 @@
 // 5-->4 6 
 // 6-->5
 
-class HashTable {
-  constructor(size){
-    this.data = new Array(size);
-  }
+// class HashTable {
+//   constructor(size){
+//     this.data = new Array(size);
+//   }
 
-  _hash(key) {
-    let hash = 0;
-    for (let i =0; i < key.length; i++){
-        hash = (hash + key.charCodeAt(i) * i) % this.data.length
-    }
-    return hash;
-  }
-  set(key, value) {
-    let address = this._hash(key)
-    if(!this.data[address]) this.data[address] = []
-    this.data[address].push([key,value])
-    return this
-  }
-  get(key) {
-    let address = this._hash(key)
-    if(this.data[address]) {
-      return this.data[address]
-    }
-  }
-}
+//   _hash(key) {
+//     let hash = 0;
+//     for (let i =0; i < key.length; i++){
+//         hash = (hash + key.charCodeAt(i) * i) % this.data.length
+//     }
+//     return hash;
+//   }
+//   set(key, value) {
+//     let address = this._hash(key)
+//     if(!this.data[address]) this.data[address] = []
+//     this.data[address].push([key,value])
+//     return this
+//   }
+//   get(key) {
+//     let address = this._hash(key)
+//     if(this.data[address]) {
+//       return this.data[address]
+//     }
+//   }
+// }
 
-const myHashTable = new HashTable(50);
-console.log(myHashTable.set('grapes', 10000))
-console.log(myHashTable.set('apples', 9))
-console.log(myHashTable.get('grapes'))
-console.log(myHashTable.get('apples'))
+// const myHashTable = new HashTable(50);
+// console.log(myHashTable.set('grapes', 10000))
+// console.log(myHashTable.set('apples', 9))
+// console.log(myHashTable.get('grapes'))
+// console.log(myHashTable.get('apples'))
+
+
+// const factoralRecursive = num => {
+//   if(num < 3) return num 
+//   return factoralRecursive(num -1) * num 
+// }
+
+// console.log(factoralRecursive(8))
