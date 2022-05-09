57552c51806cd8b319b9699ed125ae7769733fdd
andershup <anders242@gmail.com>
2022-05-09 08:50:10 +0000

    add array practice

--- main.js
+++ main.js
@@ -62,3 +62,199 @@ const bubbleSort = array =>
 }
 
 console.log(bubbleSort(array))
+
+const array = [22,22,1,76,8,4]
+
+const insertionSort = array =>
+{
+    for(i = 0 ; i < array.length ; i ++)
+    {
+        if(array[i] < array[0])
+        {
+            array.unshift(array.splice(i,1)[0])
+        }
+        if(array[i] < array[i-1])
+        {
+            for(j = 1 ; j < i ; j ++)
+            {
+                if(array[i] >= array[j-1] && array[i] < array[j])
+                {
+                    array.splice(j,0, array.splice(i,1)[0])
+                }
+            }
+        }
+    } return array 
+}
+
+console.log(insertionSort(array))
+
+const selectionSort = array =>
+{
+    for(i = 0 ; i < array.length ; i ++)
+    {
+        let min = i
+        let temp = array[i]
+        for(j = i + 1 ; j < array.length ; j ++)
+        {
+            if(array[j] < array[min])
+            {
+                min = j 
+            }
+        }
+        array[i] = min
+        array[min] = temp
+    }
+    return array 
+}
+
+console.log(selectionSort(array))
+
+const bubbleSort = array =>
+{
+    for(i = 0 ; i < array.length ; i++)
+    {
+        for(j= 0; j < array.length ; j ++)
+        {
+            if(array[j] > array[i])
+            {
+                let temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+            }
+        }
+    }
+    return array
+}
+
+console.log(bubbleSort(array))
+
+
+const array = [22,22,1,76,8,4]
+
+const insertionSort = array =>
+{
+    for(i = 0 ; i < array.length ; i ++)
+    {
+        if(array[i] < array[0])
+        {
+            array.unshift(array.splice(i,1)[0])
+        }
+        if(array[i] < array[i-1])
+        {
+            for(j = 1 ; j < i ; j ++)
+            {
+                if(array[i] >= array[j-1] && array[i] < array[j])
+                {
+                    array.splice(j,0, array.splice(i,1)[0])
+                }
+            }
+        }
+    } return array 
+}
+
+console.log(insertionSort(array))
+
+const selectionSort = array =>
+{
+    for(i = 0 ; i < array.length ; i ++)
+    {
+        let min = i
+        let temp = array[i]
+        for(j = i + 1 ; j < array.length ; j ++)
+        {
+            if(array[j] < array[min])
+            {
+                min = j 
+            }
+        }
+        array[i] = min
+        array[min] = temp
+    }
+    return array 
+}
+
+console.log(selectionSort(array))
+
+const bubbleSort = array =>
+{
+    for(i = 0 ; i < array.length ; i++)
+    {
+        for(j= 0; j < array.length ; j ++)
+        {
+            if(array[j] > array[i])
+            {
+                let temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+            }
+        }
+    }
+    return array
+}
+
+console.log(bubbleSort(array))
+
+const array = [22,22,1,76,8,4]
+
+const insertionSort = array =>
+{
+    for(i = 0 ; i < array.length ; i ++)
+    {
+        if(array[i] < array[0])
+        {
+            array.unshift(array.splice(i,1)[0])
+        }
+        if(array[i] < array[i-1])
+        {
+            for(j = 1 ; j < i ; j ++)
+            {
+                if(array[i] >= array[j-1] && array[i] < array[j])
+                {
+                    array.splice(j,0, array.splice(i,1)[0])
+                }
+            }
+        }
+    } return array 
+}
+
+console.log(insertionSort(array))
+
+const selectionSort = array =>
+{
+    for(i = 0 ; i < array.length ; i ++)
+    {
+        let min = i
+        let temp = array[i]
+        for(j = i + 1 ; j < array.length ; j ++)
+        {
+            if(array[j] < array[min])
+            {
+                min = j 
+            }
+        }
+        array[i] = min
+        array[min] = temp
+    }
+    return array 
+}
+
+console.log(selectionSort(array))
+
+const bubbleSort = array =>
+{
+    for(i = 0 ; i < array.length ; i++)
+    {
+        for(j= 0; j < array.length ; j ++)
+        {
+            if(array[j] > array[i])
+            {
+                let temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+            }
+        }
+    }
+    return array
+}
+
+console.log(bubbleSort(array))
