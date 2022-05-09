964814de2f7c757edc1e781c322d3d439d08a07f
andershup <anders242@gmail.com>
2022-05-09 09:16:30 +0000

    add object reduce method

--- main.js
+++ main.js
@@ -1,260 +1,297 @@
-const array = [22,22,1,76,8,4]
+// const array = [22,22,1,76,8,4]
 
-const insertionSort = array =>
-{
-    for(i = 0 ; i < array.length ; i ++)
+// const insertionSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i ++)
+//     {
+//         if(array[i] < array[0])
+//         {
+//             array.unshift(array.splice(i,1)[0])
+//         }
+//         if(array[i] < array[i-1])
+//         {
+//             for(j = 1 ; j < i ; j ++)
+//             {
+//                 if(array[i] >= array[j-1] && array[i] < array[j])
+//                 {
+//                     array.splice(j,0, array.splice(i,1)[0])
+//                 }
+//             }
+//         }
+//     } return array 
+// }
+
+// console.log(insertionSort(array))
+
+// const selectionSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i ++)
+//     {
+//         let min = i
+//         let temp = array[i]
+//         for(j = i + 1 ; j < array.length ; j ++)
+//         {
+//             if(array[j] < array[min])
+//             {
+//                 min = j 
+//             }
+//         }
+//         array[i] = min
+//         array[min] = temp
+//     }
+//     return array 
+// }
+
+// console.log(selectionSort(array))
+
+// const bubbleSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i++)
+//     {
+//         for(j= 0; j < array.length ; j ++)
+//         {
+//             if(array[j] > array[i])
+//             {
+//                 let temp = array[i]
+//                 array[i] = array[j]
+//                 array[j] = temp
+//             }
+//         }
+//     }
+//     return array
+// }
+
+// console.log(bubbleSort(array))
+
+// const array = [22,22,1,76,8,4]
+
+// const insertionSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i ++)
+//     {
+//         if(array[i] < array[0])
+//         {
+//             array.unshift(array.splice(i,1)[0])
+//         }
+//         if(array[i] < array[i-1])
+//         {
+//             for(j = 1 ; j < i ; j ++)
+//             {
+//                 if(array[i] >= array[j-1] && array[i] < array[j])
+//                 {
+//                     array.splice(j,0, array.splice(i,1)[0])
+//                 }
+//             }
+//         }
+//     } return array 
+// }
+
+// console.log(insertionSort(array))
+
+// const selectionSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i ++)
+//     {
+//         let min = i
+//         let temp = array[i]
+//         for(j = i + 1 ; j < array.length ; j ++)
+//         {
+//             if(array[j] < array[min])
+//             {
+//                 min = j 
+//             }
+//         }
+//         array[i] = min
+//         array[min] = temp
+//     }
+//     return array 
+// }
+
+// console.log(selectionSort(array))
+
+// const bubbleSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i++)
+//     {
+//         for(j= 0; j < array.length ; j ++)
+//         {
+//             if(array[j] > array[i])
+//             {
+//                 let temp = array[i]
+//                 array[i] = array[j]
+//                 array[j] = temp
+//             }
+//         }
+//     }
+//     return array
+// }
+
+// console.log(bubbleSort(array))
+
+
+// const array = [22,22,1,76,8,4]
+
+// const insertionSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i ++)
+//     {
+//         if(array[i] < array[0])
+//         {
+//             array.unshift(array.splice(i,1)[0])
+//         }
+//         if(array[i] < array[i-1])
+//         {
+//             for(j = 1 ; j < i ; j ++)
+//             {
+//                 if(array[i] >= array[j-1] && array[i] < array[j])
+//                 {
+//                     array.splice(j,0, array.splice(i,1)[0])
+//                 }
+//             }
+//         }
+//     } return array 
+// }
+
+// console.log(insertionSort(array))
+
+// const selectionSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i ++)
+//     {
+//         let min = i
+//         let temp = array[i]
+//         for(j = i + 1 ; j < array.length ; j ++)
+//         {
+//             if(array[j] < array[min])
+//             {
+//                 min = j 
+//             }
+//         }
+//         array[i] = min
+//         array[min] = temp
+//     }
+//     return array 
+// }
+
+// console.log(selectionSort(array))
+
+// const bubbleSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i++)
+//     {
+//         for(j= 0; j < array.length ; j ++)
+//         {
+//             if(array[j] > array[i])
+//             {
+//                 let temp = array[i]
+//                 array[i] = array[j]
+//                 array[j] = temp
+//             }
+//         }
+//     }
+//     return array
+// }
+
+// console.log(bubbleSort(array))
+
+// const array = [22,22,1,76,8,4]
+
+// const insertionSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i ++)
+//     {
+//         if(array[i] < array[0])
+//         {
+//             array.unshift(array.splice(i,1)[0])
+//         }
+//         if(array[i] < array[i-1])
+//         {
+//             for(j = 1 ; j < i ; j ++)
+//             {
+//                 if(array[i] >= array[j-1] && array[i] < array[j])
+//                 {
+//                     array.splice(j,0, array.splice(i,1)[0])
+//                 }
+//             }
+//         }
+//     } return array 
+// }
+
+// console.log(insertionSort(array))
+
+// const selectionSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i ++)
+//     {
+//         let min = i
+//         let temp = array[i]
+//         for(j = i + 1 ; j < array.length ; j ++)
+//         {
+//             if(array[j] < array[min])
+//             {
+//                 min = j 
+//             }
+//         }
+//         array[i] = min
+//         array[min] = temp
+//     }
+//     return array 
+// }
+
+// console.log(selectionSort(array))
+
+// const bubbleSort = array =>
+// {
+//     for(i = 0 ; i < array.length ; i++)
+//     {
+//         for(j= 0; j < array.length ; j ++)
+//         {
+//             if(array[j] > array[i])
+//             {
+//                 let temp = array[i]
+//                 array[i] = array[j]
+//                 array[j] = temp
+//             }
+//         }
+//     }
+//     return array
+// }
+
+// console.log(bubbleSort(array))
+
+
+ const categories = [
     {
-        if(array[i] < array[0])
-        {
-            array.unshift(array.splice(i,1)[0])
-        }
-        if(array[i] < array[i-1])
-        {
-            for(j = 1 ; j < i ; j ++)
-            {
-                if(array[i] >= array[j-1] && array[i] < array[j])
-                {
-                    array.splice(j,0, array.splice(i,1)[0])
-                }
-            }
-        }
-    } return array 
-}
-
-console.log(insertionSort(array))
-
-const selectionSort = array =>
-{
-    for(i = 0 ; i < array.length ; i ++)
+      id: 1,
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
     {
-        let min = i
-        let temp = array[i]
-        for(j = i + 1 ; j < array.length ; j ++)
-        {
-            if(array[j] < array[min])
-            {
-                min = j 
-            }
-        }
-        array[i] = min
-        array[min] = temp
-    }
-    return array 
-}
-
-console.log(selectionSort(array))
-
-const bubbleSort = array =>
-{
-    for(i = 0 ; i < array.length ; i++)
+      id: 2,
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
     {
-        for(j= 0; j < array.length ; j ++)
-        {
-            if(array[j] > array[i])
-            {
-                let temp = array[i]
-                array[i] = array[j]
-                array[j] = temp
-            }
-        }
-    }
-    return array
-}
-
-console.log(bubbleSort(array))
-
-const array = [22,22,1,76,8,4]
-
-const insertionSort = array =>
-{
-    for(i = 0 ; i < array.length ; i ++)
+      id: 3,
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
     {
-        if(array[i] < array[0])
-        {
-            array.unshift(array.splice(i,1)[0])
-        }
-        if(array[i] < array[i-1])
-        {
-            for(j = 1 ; j < i ; j ++)
-            {
-                if(array[i] >= array[j-1] && array[i] < array[j])
-                {
-                    array.splice(j,0, array.splice(i,1)[0])
-                }
-            }
-        }
-    } return array 
-}
-
-console.log(insertionSort(array))
-
-const selectionSort = array =>
-{
-    for(i = 0 ; i < array.length ; i ++)
+      id: 4,
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
     {
-        let min = i
-        let temp = array[i]
-        for(j = i + 1 ; j < array.length ; j ++)
-        {
-            if(array[j] < array[min])
-            {
-                min = j 
-            }
-        }
-        array[i] = min
-        array[min] = temp
-    }
-    return array 
-}
+      id: 5,
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
 
-console.log(selectionSort(array))
+  const tryThis = categories.reduce((acc, {title,id}) => {
+      acc [title.toLocaleLowerCase()]= items 
 
-const bubbleSort = array =>
-{
-    for(i = 0 ; i < array.length ; i++)
-    {
-        for(j= 0; j < array.length ; j ++)
-        {
-            if(array[j] > array[i])
-            {
-                let temp = array[i]
-                array[i] = array[j]
-                array[j] = temp
-            }
-        }
-    }
-    return array
-}
+      return acc
+  })
 
-console.log(bubbleSort(array))
-
-
-const array = [22,22,1,76,8,4]
-
-const insertionSort = array =>
-{
-    for(i = 0 ; i < array.length ; i ++)
-    {
-        if(array[i] < array[0])
-        {
-            array.unshift(array.splice(i,1)[0])
-        }
-        if(array[i] < array[i-1])
-        {
-            for(j = 1 ; j < i ; j ++)
-            {
-                if(array[i] >= array[j-1] && array[i] < array[j])
-                {
-                    array.splice(j,0, array.splice(i,1)[0])
-                }
-            }
-        }
-    } return array 
-}
-
-console.log(insertionSort(array))
-
-const selectionSort = array =>
-{
-    for(i = 0 ; i < array.length ; i ++)
-    {
-        let min = i
-        let temp = array[i]
-        for(j = i + 1 ; j < array.length ; j ++)
-        {
-            if(array[j] < array[min])
-            {
-                min = j 
-            }
-        }
-        array[i] = min
-        array[min] = temp
-    }
-    return array 
-}
-
-console.log(selectionSort(array))
-
-const bubbleSort = array =>
-{
-    for(i = 0 ; i < array.length ; i++)
-    {
-        for(j= 0; j < array.length ; j ++)
-        {
-            if(array[j] > array[i])
-            {
-                let temp = array[i]
-                array[i] = array[j]
-                array[j] = temp
-            }
-        }
-    }
-    return array
-}
-
-console.log(bubbleSort(array))
-
-const array = [22,22,1,76,8,4]
-
-const insertionSort = array =>
-{
-    for(i = 0 ; i < array.length ; i ++)
-    {
-        if(array[i] < array[0])
-        {
-            array.unshift(array.splice(i,1)[0])
-        }
-        if(array[i] < array[i-1])
-        {
-            for(j = 1 ; j < i ; j ++)
-            {
-                if(array[i] >= array[j-1] && array[i] < array[j])
-                {
-                    array.splice(j,0, array.splice(i,1)[0])
-                }
-            }
-        }
-    } return array 
-}
-
-console.log(insertionSort(array))
-
-const selectionSort = array =>
-{
-    for(i = 0 ; i < array.length ; i ++)
-    {
-        let min = i
-        let temp = array[i]
-        for(j = i + 1 ; j < array.length ; j ++)
-        {
-            if(array[j] < array[min])
-            {
-                min = j 
-            }
-        }
-        array[i] = min
-        array[min] = temp
-    }
-    return array 
-}
-
-console.log(selectionSort(array))
-
-const bubbleSort = array =>
-{
-    for(i = 0 ; i < array.length ; i++)
-    {
-        for(j= 0; j < array.length ; j ++)
-        {
-            if(array[j] > array[i])
-            {
-                let temp = array[i]
-                array[i] = array[j]
-                array[j] = temp
-            }
-        }
-    }
-    return array
-}
-
-console.log(bubbleSort(array))
+  console.log(tryThis)
